61ca3908201cc3bea8bcd33ec02f310b1bc348d8
henzlym <45774619+henzlym@users.noreply.github.com>
2024-07-21 14:41:42 -0400

    update image lazy loading

--- inc/media.php
+++ inc/media.php
@@ -5,7 +5,43 @@ function bca_wp_content_img_tag(string $filtered_image, string $context, int $at
 	return $filtered_image;
 }
 add_filter('wp_content_img_tag', 'bca_wp_content_img_tag', 10, 3);
+/**
+ * Filters the HTML img element representing an image attachment.
+ *
+ * @since 5.6.0
+ *
+ * @param string       $html          HTML img element or empty string on failure.
+ * @param int          $attachment_id Image attachment ID.
+ * @param string|int[] $size          Requested image size. Can be any registered image size name, or
+ *                                    an array of width and height values in pixels (in that order).
+ * @param bool         $icon          Whether the image should be treated as an icon.
+ * @param string[]     $attr          Array of attribute values for the image markup, keyed by attribute name.
+ *                                    See wp_get_attachment_image().
+ */
+function bca_wp_get_attachment_image(string $html, int $attachment_id, string $size): string
+{
+	$html = replace_img_src_with_data_src($html);
+	return $html;
+}
+// add_filter('wp_get_attachment_image', 'bca_wp_get_attachment_image', 10, 3);
 
+/**
+ * Filters the post thumbnail HTML to use the Picture element.
+ *
+ * @param string $html              The post thumbnail HTML.
+ * @param int    $post_id           The post ID.
+ * @param int    $post_thumbnail_id The post thumbnail ID.
+ * @param string $size              The post thumbnail size.
+ * @param array  $attr              Array of attribute values for the post thumbnail.
+ *
+ * @return string Modified post thumbnail HTML.
+ */
+function bca_post_thumbnail_html($html, $post_id, $post_thumbnail_id, $size, $attr)
+{
+	$html = replace_img_src_with_data_src($html);
+	return $html;
+}
+add_filter('post_thumbnail_html', 'bca_post_thumbnail_html', 10, 5);
 /**
  * Filters the list of attachment image attributes.
  *
@@ -19,11 +55,8 @@ add_filter('wp_content_img_tag', 'bca_wp_content_img_tag', 10, 3);
  */
 function wpdocs_filter_gallery_img_atts($atts, $attachment, $size)
 {
-	$placeholder = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDI0IiB2aWV3Qm94PSIwIDAgMTAyNCAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaWRZTWlkIG1lZXQiPjxyZWN0IHdpZHRoPSIxMDI0IiBoZWlnaHQ9IjIwMCIgZmlsbD0iI2NjY2NjYyIgLz48L3N2Zz4=";
-	if (!isset($atts['data-src'])) {
-		$atts['data-src'] = $atts['src'];
-		$atts['src'] = $placeholder;
-	}
+	unset($atts['srcset']);
+	unset($atts['sizes']);
 	return $atts;
 }
 add_filter('wp_get_attachment_image_attributes', 'wpdocs_filter_gallery_img_atts', 10, 3);
@@ -69,7 +102,7 @@ function bca_add_lazyload_styles()
 function bca_add_lazyload_scripts()
 {
 ?>
-	<script>
+	<script id="bca-add-lazyload-scripts">
 		document.addEventListener("DOMContentLoaded", function() {
 			const lazyImages = document.querySelectorAll('img[data-src]');
 
